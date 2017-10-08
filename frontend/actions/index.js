efc4573e350ab2dbc601d45d63cebf44ee5b6d1f
Gustavo Brunetto <gustavobruneto@gmail.com>
2017-10-08 17:32:05 -0300

    func: initial commit and add actions ADD_POST DELETE_POST

---
+++ frontend/actions/index.js
@@ -0,0 +1,27 @@
+export const ADD_POST = 'ADD_POST'
+export const DELETE_POST = 'DELETE_POST'
+export const ADD_COMMENT = 'ADD_COMMENT'
+export const DELETE_COMMENT = 'DELETE_COMMENT'
+export const 
+
+export function addPost({ id, timestamp, title, body, author, category, voteScore, deleted }) {
+    return {
+        type: ADD_POST,
+        id,
+        timestamp, 
+        title,
+        body,
+        author,
+        category,
+        voteScore,
+        deleted
+    }
+}
+
+export function deletePost({ id }) {
+    return {
+        type: DELETE_POST,
+        id,
+        deleted: true
+    }
+}
