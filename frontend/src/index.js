7583fc3e405e45185736cb6d05790189a15fbd94
Gustavo Brunetto <gustavobruneto@gmail.com>
2017-10-09 16:41:42 -0300

    chore: move components to components folder

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import App from './components/App';
 import registerServiceWorker from './registerServiceWorker';
 import { createStore } from 'redux';
 import reducer from './reducers';
@@ -9,9 +9,11 @@ import reducer from './reducers';
 const store = createStore(
     reducer,
     window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+); 
+
+ReactDOM.render(
+    <App store={ store }/>, 
+    document.getElementById('root')
 );
 
-console.log(store.getState()); 
-
-ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
