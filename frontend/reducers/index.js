1afefea7e7303cfc2f2f4840cb700bc44dabf661
Gustavo Brunetto <gustavobruneto@gmail.com>
2017-10-09 15:27:53 -0300

    fix: change initialPostState

--- frontend/reducers/index.js
+++ frontend/reducers/index.js
@@ -3,16 +3,18 @@ import {
     DELETE_POST
 } from '../actions'
 
-const initialPostState = []
+const initialPostState = {
+    posts: [],
+}
 
 const post = (state = initialPostState, action) => {
     const { id, timestamp, title, body, author, category, voteScore, deleted } = action.post
     
     switch(action.type) {
         case ADD_POST:
-            return [...initialPostState, action.post]
+            return [...initialPostState.posts, action.post]
         case DELETE_POST:
-            return state.filter((post) => post.id !== action.post.id)
+            return state.posts.filter((post) => post.id !== action.post.id)
         default:
             return state
     }
