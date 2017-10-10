9b3f69e5efe28f641cc53aa0e97ef44609f32eed
Gustavo Brunetto <gustavobruneto@gmail.com>
2017-10-10 09:11:29 -0300

    chore: add comments

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -12,17 +12,20 @@ class App extends Component {
   componentDidMount() {
     const { store } = this.props;
 
+    // redux store: watching for changes
     store.subscribe(() => {
       this.setState(() => ({
         postsList: store.getState()
       }))
-    })
+    });
+
   }
 
   submitPost = () => {
+    // redux store: dispatch
     this.props.store.dispatch(addPost({
       id: this.input.value,
-      timestamp: 'timestamp', 
+      timestamp: 'timestamp',
       title: 'post title',
       body: 'none',
       author: 'author',
@@ -43,23 +46,30 @@ class App extends Component {
     return (
       <div className="App">
         <SimpleAppBar title="My Posts"/>
-        
+
+        {/* TODO: Input form - open another window */}
+        {/* TODO: create a post component (local state thumbs up/down) */}
         <input
           type="text"
           ref={(input) => this.input = input}
           placeholder="type text"
         />
         <button onClick={this.submitPost}>Submit</button>
-        
+
+        {/* Show posts */}
         <pre>
-          We have a {this.state.postsList && this.state.postsList.map((item) => (
-            <div key={item.id}>
-              <li>{item.id}</li>
-              <button onClick={() => this.removePost(item.id)}>X</button>
-            </div>
+          We have {this.state.postsList && this.state.postsList.length} posts
+        </pre>
+        <pre>
+          {this.state.postsList &&
+            this.state.postsList.map((item) => (
+              <div key={item.id}>
+                <li>{item.id}</li>
+                <button onClick={() => this.removePost(item.id)}>X</button>
+              </div>
           ))}
         </pre>
-        
+
         {/* <EnhancedTable /> */}
       </div>
     );
