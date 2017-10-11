a2e24bcd83ac6736db07c4a34be9ff7495c079ec
Gustavo Brunetto <gustavobruneto@gmail.com>
2017-10-11 17:05:03 -0300

    func: start implement react-redux

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -1,18 +1,36 @@
 import React, { Component } from 'react';
 import SimpleAppBar from './AppBar';
 import EnhancedTable from './AppTables';
+import { connect } from 'react-redux';
 import { addPost, deletePost } from '../actions'
 import './App.css';
 
 class App extends Component {
-  
+  includePost = () => {
+    this.props.insertPost({
 
+    });
+  }
   render() {
+    console.log('Props', this.props);
     return (
       <div className="App">
         <SimpleAppBar title="My Posts"/>
 
-        Hello World
+        <p>
+          We have {this.props.postList.length} posts
+        </p>
+
+        <div>
+          {this.props.postList.map((item) => (
+            <li key={item.id}>{item.body}</li>
+          ))}
+        </div>
+
+        <div>
+          <input type='text' ></input>
+          <button>Submit</button>
+        </div>
 
         {/* <EnhancedTable /> */}
       </div>
@@ -20,4 +38,16 @@ class App extends Component {
   }
 }
 
-export default App;
+// connect component to redux Store
+const mapStateToProps = (postList) => (
+  {postList}
+)
+
+const mapDispatchToProps = (dispatch) => (
+  {
+    insertPost: (data) => dispatch(addPost(data)),
+    removePost: (id) => dispatch(deletePost(id))
+  }
+)
+
+export default connect(mapStateToProps, mapDispatchToProps)(App);
