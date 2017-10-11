11cdbd0805bba634fed16d75a6e34ebc0786352c
Gustavo Brunetto <gustavobruneto@gmail.com>
2017-10-11 17:04:42 -0300

    style: standarized the tab space

--- frontend/src/actions/index.js
+++ frontend/src/actions/index.js
@@ -3,27 +3,27 @@ export const DELETE_POST = 'DELETE_POST';
 export const ADD_COMMENT = 'ADD_COMMENT';
 export const DELETE_COMMENT = 'DELETE_COMMENT';
 
-export function addPost({ id, timestamp, title, body, author, category, voteScore, deleted }) {
-    return {
-        type: ADD_POST,
-        post: {
-            id,
-            timestamp, 
-            title,
-            body,
-            author,
-            category,
-            voteScore,
-            deleted
-        }
+export const addPost = ({ id, timestamp, title, body, author, category, voteScore, deleted }) => (
+  {
+    type: ADD_POST,
+    post: {
+      id,
+      timestamp, 
+      title,
+      body,
+      author,
+      category,
+      voteScore,
+      deleted
     }
-}
+  }
+);
 
-export function deletePost({ id }) {
-    return {
-        type: DELETE_POST,
-        post: {
-            id
-        }
+export const deletePost = ({ id }) => (
+  {
+    type: DELETE_POST,
+    post: {
+      id
     }
-}
+  }
+);
--- frontend/src/reducers/index.js
+++ frontend/src/reducers/index.js
@@ -1,22 +1,43 @@
 import {
-    ADD_POST,
-    DELETE_POST
-} from '../actions'
+  ADD_POST,
+  DELETE_POST
+} from '../actions';
 
-const initialPostsState = [];
+const initialPostsState = [
+  {
+    id: '1',
+    timestamp: '3498534', 
+    title: 'post-title',
+    body: 'post body',
+    author: 'post author',
+    category: 'redux',
+    voteScore: 1,
+    deleted: false
+  },
+  {
+    id: '2',
+    timestamp: '3498534', 
+    title: 'post-title',
+    body: 'post body',
+    author: 'post author',
+    category: 'redux',
+    voteScore: 1,
+    deleted: false
+  }
+];
 
 const post = (state = initialPostsState, action) => {
-    //const { id, timestamp, title, body, author, category, voteScore, deleted } = action
-    const currentPost = action.post;
+  //const { id, timestamp, title, body, author, category, voteScore, deleted } = action
+  const currentPost = action.post;
     
-    switch(action.type) {
-        case ADD_POST:
-            return [...state, currentPost]
-        case DELETE_POST:
-            return state.filter((item) => item.id !== currentPost.id)
-        default:
-            return state
-    }
+  switch(action.type) {
+    case ADD_POST:
+      return [...state, currentPost];
+    case DELETE_POST:
+      return state.filter((item) => item.id !== currentPost.id);
+    default:
+      return state;
+  }
 }
 
-export default post
+export default post;
