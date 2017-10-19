b8994633d8a57b32f84a1a6343f2359b6ecb4a3d
Gustavo Brunetto <gustavobruneto@gmail.com>
2017-10-19 17:31:18 -0200

    func: add modal to insert new post

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -2,34 +2,147 @@ import React, { Component } from 'react';
 import SimpleAppBar from './AppBar';
 import EnhancedTable from './AppTables';
 import { connect } from 'react-redux';
-import { addPost, deletePost } from '../actions'
+import { addPost, deletePost } from '../actions';
+import Modal from 'react-modal';
+import { fetchPosts } from '../utils/api';
 import './App.css';
 
-class App extends Component {
-  includePost = () => {
-    this.props.insertPost({
-
-    });
+const customStyles = {
+  content : {
+    top                   : '50%',
+    left                  : '50%',
+    right                 : 'auto',
+    bottom                : 'auto',
+    marginRight           : '-50%',
+    transform             : 'translate(-50%, -50%)'
   }
+};
+
+class App extends Component {
+
+  state = {
+    modalIsOpen: false
+  };
+
+  openModal = () => {
+    this.setState(() => ({
+      modalIsOpen: true
+    }));
+  }
+
+  afterOpenModal() {
+    // references are now sync'd and can be accessed.
+    this.subtitle.style.color = '#f00';
+  }
+
+  closeModal = () => {
+    this.setState(() => ({
+      modalIsOpen: false
+    }))
+  }
+  
+  includePost = (e) => {
+    if (!this.input.value) {
+      return;
+    }
+
+    e.preventDefault();
+
+    this.props.insertPost({
+        id:"bla",
+        timestamp:"bla", 
+        title:"bla",
+        body: this.input.value,
+        author:"bla",
+        category:"bla",
+        voteScore:"bla",
+        deleted:"bla"
+    });
+
+    this.input.value = '';
+
+    if (this.state.modalIsOpen) {
+      this.closeModal();
+    }
+
+  }
+
+  removePost = () => {
+    this.props.deletePost(
+      {}
+    )
+  }
+  
+  retrievePosts = () => async (dispatch) => {
+    console.log('Trying to retrieve the posts...');
+    try {
+      const posts = await fetchPosts();
+      console.log(posts);
+      posts.map((item) => {
+        this.props.insertPost(item);
+      })
+    } catch(err) {
+      console.error("Error retrieving posts...", err)
+    }
+  }
+
+  
+
+  
+
   render() {
     console.log('Props', this.props);
+
     return (
       <div className="App">
         <SimpleAppBar title="My Posts"/>
 
         <p>
-          We have {this.props.postList.length} posts
+          We have {this.props.postList.length ? this.props.postList.length : 0} posts
         </p>
 
         <div>
-          {this.props.postList.map((item) => (
-            <li key={item.id}>{item.body}</li>
-          ))}
+          <button
+            onClick={this.openModal}>
+            Create New Post
+          </button>
         </div>
 
         <div>
-          <input type='text' ></input>
-          <button>Submit</button>
+          {this.props.postList.map((item) => (
+            <li key={item.id}>{item.title}</li>
+          ))}
+        </div>
+
+        <Modal
+          isOpen={this.state.modalIsOpen}
+          onRequestClose={this.closeModal}
+          contentLabel='Modal'>
+            <div>
+              <div>
+                <input
+                  type='text'
+                  placeholder='Post text...'
+                  ref={(input) => this.input = input}
+                />
+              </div>
+
+              <div>
+                <button 
+                  onClick={this.includePost}>
+                    Post
+                </button>
+              
+              <button
+                onClick={this.closeModal}>
+                Cancel
+              </button>
+              </div>
+            </div>
+        </Modal>
+
+        <div>
+          <button onClick={this.retrievePosts()}>fetch</button>
         </div>
 
         {/* <EnhancedTable /> */}
@@ -39,8 +152,11 @@ class App extends Component {
 }
 
 // connect component to redux Store
-const mapStateToProps = (postList) => (
-  {postList}
+const mapStateToProps = (postList, commentList) => (
+  {
+    postList: postList.post,
+    commentList: postList.comment
+  }
 )
 
 const mapDispatchToProps = (dispatch) => (
